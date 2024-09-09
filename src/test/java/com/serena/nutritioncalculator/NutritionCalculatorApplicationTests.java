4eebb207842679914c0f7566934a27ed4b7086ff
Serena <serenalin1997@gmail.com>
2024-09-09 16:30:35 +0800

    初始化spring boot project

---
+++ src/test/java/com/serena/nutritioncalculator/NutritionCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serena.nutritioncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
