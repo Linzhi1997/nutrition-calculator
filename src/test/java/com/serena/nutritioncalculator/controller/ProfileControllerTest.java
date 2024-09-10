bbd90bac9331796057895d1eadf6b866936b3c6e
Serena <serenalin1997@gmail.com>
2024-09-10 17:18:20 +0800

    單元測試 - 基本資料表

---
+++ src/test/java/com/serena/nutritioncalculator/controller/ProfileControllerTest.java
@@ -0,0 +1,104 @@
+package com.serena.nutritioncalculator.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.serena.nutritioncalculator.dao.ProfileDao;
+import com.serena.nutritioncalculator.dto.ProfileCreateRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProfileControllerTest {
+    @Autowired
+    ProfileDao profileDao;
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void create_success() throws Exception{
+        // 第一次創建
+        ProfileCreateRequest profileCreateRequest = new ProfileCreateRequest();
+        profileCreateRequest.setWeight(60.0f);
+        profileCreateRequest.setHeight(160f);
+        profileCreateRequest.setActivity(1.2f);
+        profileCreateRequest.setGoalWeight(52.0f);
+
+        String json = objectMapper.writeValueAsString(profileCreateRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/1/profiles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId", equalTo(1)))
+                .andExpect(jsonPath("$.age",notNullValue()))
+                .andExpect(jsonPath("$.height",equalTo(160.0)))
+                .andExpect(jsonPath("$.weight",equalTo(60.0)))
+                .andExpect(jsonPath("$.bmi",notNullValue()))
+                .andExpect(jsonPath("$.activity",equalTo(1.2)))
+                .andExpect(jsonPath("$.bmr",notNullValue()))
+                .andExpect(jsonPath("$.tdee",notNullValue()))
+                .andExpect(jsonPath("$.goalWeight",equalTo(52.0)));
+    }
+
+    @Test
+    public void create_second_time_success() throws Exception{
+        ProfileCreateRequest profileCreateRequest = new ProfileCreateRequest();
+        profileCreateRequest.setWeight(78.0f);
+
+        String json = objectMapper.writeValueAsString(profileCreateRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/2/profiles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.userId", equalTo(2)))
+                .andExpect(jsonPath("$.age",notNullValue()))
+                .andExpect(jsonPath("$.height",equalTo(178.0)))
+                .andExpect(jsonPath("$.weight",equalTo(78.0)))
+                .andExpect(jsonPath("$.bmi",notNullValue()))
+                .andExpect(jsonPath("$.activity",equalTo(1.2)))
+                .andExpect(jsonPath("$.bmr",notNullValue()))
+                .andExpect(jsonPath("$.tdee",notNullValue()))
+                .andExpect(jsonPath("$.goalWeight",equalTo(72.0)));
+    }
+
+    @Test
+    public void create_invalidUser() throws Exception {
+        // 使用不存在的user創建
+        ProfileCreateRequest profileCreateRequest = new ProfileCreateRequest();
+        profileCreateRequest.setWeight(80.44f);
+        profileCreateRequest.setHeight(175f);
+        profileCreateRequest.setActivity(1.2f);
+        profileCreateRequest.setGoalWeight(70f);
+
+        String json = objectMapper.writeValueAsString(profileCreateRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/users/100/profiles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+}
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,3 +3,6 @@ INSERT INTO users (user_name,email, password, sex, birth, user_created_date)
 VALUES ('user1','user1@gmail.com', '202cb962ac59075b964b07152d234b70', 'F','1999-12-31', '2024-09-10');
 INSERT INTO users (user_name,email, password, sex, birth, user_created_date)
 VALUES ('user2','user2@gmail.com', '202cb962ac59075b964b07152d234b70', 'M','1999-01-31', '2024-09-10');
+-- profile
+INSERT INTO profile (user_id,age,height,weight,bmi,activity,bmr,tdee,goal_weight,expected_weight_change,profile_created_date)
+VALUES (2,25,178,90,28.4,1.2,1722,2066,72.0,'理想的體重下降速度：0.3 kg/週','2024-09-10');
